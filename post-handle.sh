833e538a79d155998dee9f7c45ccddd713f654f1
Weiliang Lin <weiliang.lin@intel.com>
2015-10-30 14:01:02 +0800

    add sunspider-1.0.2 and post handling to emit each case score

---
+++ post-handle.sh
@@ -0,0 +1,1 @@
+~/work/v8/out/x64.release/d8 sunspider-test-prefix.js $1 sunspider-analyze-results.js
--- runTest.js
+++ runTest.js
@@ -1,10 +1,12 @@
 var fs = require('fs');
 var path = require('path');
-var testVersion = '1.0.1';
+var testVersion = '1.0.2';
 var folder = path.join(process.cwd(), 'sunspider-' + testVersion);
 var files = fs.readdirSync(folder);
 var totalTime = 0, subCounter = 1;
 var vm = require('vm');
+var d = new Date();
+var timeString = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate() + "-" + d.getHours() + "." + d.getMinutes() + "." + d.getSeconds();
 
 {
   var script = fs.readFileSync("./octane.js") + "";
@@ -14,13 +16,20 @@ var vm = require('vm');
   console.log("------\n");
 }
 
+var scores = [];
+
 setInterval(function(){
   if (subCounter == 10) {
     console.log("Total SunSpider AVG. Time", totalTime/ 10);
+    var result_file = "sunspider-results-" + timeString + ".js";
+    console.log("Results are located at ./" + result_file);
+    fs.writeFileSync(result_file, "var output = ");
+    fs.appendFileSync(result_file, JSON.stringify(scores));
     process.exit(0);
   }
 
-  subCounter++;
+  var index = subCounter++;
+  var score = {};
   for(var counter = 0;counter < files.length; counter++) {
     var file = files[counter];
 
@@ -31,6 +40,8 @@ setInterval(function(){
     var context = vm.createContext(sandbox);
 
     var subTotal = vm.runInContext("var START_TIME=Date.now();" + script + ";Date.now()-START_TIME", context);
+    score[path.basename(file, ".js")] = subTotal;
     totalTime += subTotal;
   }
+  scores[index-1] = score;
 }, 123);
